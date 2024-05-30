366a16fd9f20504b1d2d92f3b5c529ddb359aba0
Luis De Ávila <luis.deavila@urosario.edu.co>
2024-05-29 20:42:04 -0500

    Boilerplate commit

---
+++ ZangelGameSyncServer/Endpoints/CheckFolder.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Dynamic;
+
+namespace ZangelGameSyncServer.Endpoints
+{
+    public class CheckFolderEndpoint
+    {
+        public static async Task<String> Get(HttpRequest request)
+        {
+            return "Hello World! " + request.Query["folderId"];
+        }
+    }
+}
