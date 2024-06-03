23c1b27e060868d1ee4930a24b7bfe19dab4c045
Luis De Ávila <luis.deavila@urosario.edu.co>
2024-06-03 17:18:52 -0500

    Starting work on CLI client

---
+++ ZangelGameSyncClient/Program.cs
@@ -0,0 +1,29 @@
+﻿static DateTime GetLatestModifiedTimestamp(string folderPath)
+{
+    // Get latest timestamp recursively
+    DateTime latestTimestamp = Directory.GetLastWriteTimeUtc(folderPath);
+    // Get latest timestamp from files
+    foreach (string filePath in Directory.GetFiles(folderPath, "*", SearchOption.AllDirectories))
+    {
+        DateTime fileTimestamp = File.GetLastWriteTimeUtc(filePath);
+        latestTimestamp = fileTimestamp > latestTimestamp ? fileTimestamp : latestTimestamp;
+    }
+
+    // get latest timestamp from folders
+    foreach (string dirPath in Directory.GetDirectories(folderPath, "*", SearchOption.AllDirectories))
+    {
+        DateTime dirTimestamp = Directory.GetLastWriteTimeUtc(dirPath);
+        dirTimestamp = dirTimestamp > latestTimestamp ? dirTimestamp : latestTimestamp;
+    }
+
+    // return it
+    return latestTimestamp;
+}
+
+string saveFolderTest = @"C:\Users\bizan\Downloads\stellaris";
+
+long timestamp = new DateTimeOffset(
+    GetLatestModifiedTimestamp(saveFolderTest)
+).ToUnixTimeSeconds();
+
+Console.WriteLine($"Timestamp: {timestamp}");
