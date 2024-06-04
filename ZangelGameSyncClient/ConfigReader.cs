1d29ce21ecd9849eb3d0e3a8a1aa72ceeafe0071
Luis De Ávila <luis.deavila@urosario.edu.co>
2024-06-03 22:15:22 -0500

    Added some more boilerplate

--- ZangelGameSyncClient/ConfigReader.cs
+++ ZangelGameSyncClient/ConfigReader.cs
@@ -2,9 +2,25 @@
 {
     internal class ConfigReader
     {
-        public ConfigReader()
-        {
 
+        public struct GameSyncConfig
+        {
+            public string remoteHost { get; init; }
+            public string localSyncFolder { get; init; }
+            public string remoteFolderId { get; init; }
+        }
+
+        public static GameSyncConfig ReadConfig()
+        {
+            throw new NotImplementedException();
+
+            // validate config
+            //ValidateConfig();
+        }
+
+        private static void ValidateConfig(GameSyncConfig config)
+        {
+            throw new NotImplementedException();
         }
     }
 }
