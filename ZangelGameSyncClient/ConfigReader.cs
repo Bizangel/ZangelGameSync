e45a4670d71f7972ddc8f3ac5dc85c869cacdf3c
Luis De Ávila <luis.deavila@urosario.edu.co>
2024-06-12 21:47:21 -0500

    WIP ExceptionHandler + ConfigReader

--- ZangelGameSyncClient/ConfigReader.cs
+++ ZangelGameSyncClient/ConfigReader.cs
@@ -1,25 +1,35 @@
 ﻿namespace ZangelGameSyncClient
 {
+    internal class SyncConfigError(string message) : Exception(message) { }
+
     internal class ConfigReader
     {
-
         public struct GameSyncConfig
         {
-            public string remoteHost { get; init; }
-            public string localSyncFolder { get; init; }
-            public string remoteFolderId { get; init; }
+            public string RemoteHost { get; init; }
+            public string LocalSyncFolder { get; init; }
+            public string RemoteFolderId { get; init; }
         }
 
         public static GameSyncConfig ReadConfig()
         {
-            throw new NotImplementedException();
 
-            // validate config
-            //ValidateConfig();
+
+
+            return new GameSyncConfig
+            {
+                LocalSyncFolder = "",
+                RemoteFolderId = "",
+                RemoteHost = "localhost",
+            };
         }
 
         private static void ValidateConfig(GameSyncConfig config)
         {
+
+            if (!Path.IsPathRooted(config.LocalSyncFolder))
+                throw new SyncConfigError("Unable to specify");
+
             throw new NotImplementedException();
         }
     }
---
+++ ZangelGameSyncClient/SyncClientExceptionHandler.cs
@@ -0,0 +1,46 @@
+﻿namespace ZangelGameSyncClient
+{
+    // TODO think of a way to rewrite this handler.
+
+    public class SyncClientExceptionHandler : IDisposable
+    {
+        private bool _disposed = false;
+
+        public SyncClientExceptionHandler()
+        {
+        }
+
+        public void Dispose()
+        {
+            if (!_disposed)
+            {
+                _disposed = true;
+                // Clean up resources if needed
+            }
+        }
+
+        public void Handle(Action action)
+        {
+            try
+            {
+                action();
+            }
+            catch (Exception ex)
+            {
+                HandleException(ex);
+            }
+        }
+
+        private void HandleException(Exception ex)
+        {
+            if (ex is SyncConfigError)
+            {
+                ConsolePrinter.Error($"Configuration error:\n{ex.Message}");
+                return;
+            }
+
+            // unhandled, rethrow
+            throw ex;
+        }
+    }
+}
